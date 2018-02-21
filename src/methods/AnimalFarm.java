c118625c0ebaee4180c836af65f0c739be89616c
codeclub <codeclub@smms-rm23-20.smusd.local>
2018-02-21 14:53:22 -0800

    AnimalFarm

--- src/methods/AnimalFarm.java
+++ src/methods/AnimalFarm.java
@@ -18,14 +18,35 @@ AnimalFarm() {
 	 * 1. Ask the user which animal they want, then play the sound of that
 	 * animal.
 	 */
-
+String animal = JOptionPane.showInputDialog("What animal sound do you want to listien too?");
 	/* 2. Make it so that the user can keep entering new animals. */
+if(animal.equals("cow")) {
+playMoo();
 }
+if(animal.equals("duck")) {
+playQuack();
+}
+if(animal.equals("dog")) {
+	playWoof();
+}
+if(animal.equals("cat")) {
+	playMeow();
+}
+if(animal.equals("llama")) {
+	playllama();
+}
+}
+
+
+
+
+
+
 
 void playMoo() {
 	playNoise(mooFile);
 }
-
+ 
 void playQuack() {
 	playNoise(quackFile);
 }
@@ -34,6 +55,12 @@ void playWoof() {
 	playNoise(woofFile);
 }
 
+void playMeow() {
+	playNoise(meowFile);
+}
+void playllama() {
+	playNoise(llamaFile);
+}
 String quackFile = "quack.wav";
 String mooFile = "moo.wav";
 String woofFile = "woof.wav";
