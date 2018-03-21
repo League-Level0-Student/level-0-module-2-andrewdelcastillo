f6eea35cb233148be8a7957f107a64b00e169bd6
codeclub <codeclub@smms-rm23-20.smusd.local>
2018-03-21 14:54:31 -0700

    StarShow

--- src/extra/SoundtrackToLife.java
+++ src/extra/SoundtrackToLife.java
@@ -13,24 +13,29 @@ public class SoundtrackToLife {
 		// 1. Adjust this pop-up to find out what mood the user is in.
 		int userMood = JOptionPane.showOptionDialog(null, "What mood are you in", "Pop-up Title", 0,
 				JOptionPane.INFORMATION_MESSAGE, null, new String[] { "happy", "angry", "sad" }, null);
-		
-		// 2. Their answer is stored in the userMood variable. Print it out.
-System.out.println(userMood);
-		// 3. If they are in a stressed mood, use the playVideo method to play a calming song from YouTube.
-		if(userMood == 0)
-{playVideo("http://bit.ly/video-for-happy");  
-		if(userMood == 1)
-			playVideo("")
-		// 4. Play different songs for other moods.
 
-/**
-* If you can’t think of any, here are some you can use...
-*	http://bit.ly/video-for-happy
-*	http://bit.ly/video-for-sad
-*	http://bit.ly/video-for-angry
-**/
-		
-		// If you are seeing ads at the beginning of your videos, install Adblock.
+		// 2. Their answer is stored in the userMood variable. Print it out.
+		System.out.println(userMood);
+		// 3. If they are in a stressed mood, use the playVideo method to play a calming
+		// song from YouTube.
+		if (userMood == 0) {
+			playVideo("http://bit.ly/video-for-happy");
+		}
+		if (userMood == 1) {
+			playVideo("http://bit.ly/video-for-sad");
+		}
+		// 4. Play different songs for other moods.
+if (userMood == 2) {
+	playVideo("http://bit.ly/video-for-angry");}
+
+}
+		/**
+		 * If you can’t think of any, here are some you can use...
+		 * http://bit.ly/video-for-happy http://bit.ly/video-for-sad
+		 * http://bit.ly/video-for-angry
+		 **/
+
+	{	// If you are seeing ads at the beginning of your videos, install Adblock.
 
 	}
 
@@ -43,6 +48,3 @@ System.out.println(userMood);
 		}
 	}
 }
-
-
-
